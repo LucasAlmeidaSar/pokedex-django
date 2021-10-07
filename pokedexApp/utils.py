7f963cca5e7bd0701d749c5d29a58e7577053212
Lucas de Almeida Sarmento <lukas.barcee@gmail.com>
2021-10-07 10:34:12 -0300

    Criando arquivo de estáticos e templates. Adicionando classes da View e definindo funções da utils.py

--- pokedexApp/utils.py
+++ pokedexApp/utils.py
@@ -1,30 +1,100 @@
+import requests
+
 def getPokemon(url_pokemon):
-  pass
+    response = requests.get(url_pokemon)
+    pokemon = response.json()
+    img = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{pokemon['id']}.png"
+    pokemon['img'] = img
+    types = [types['type']['name'].capitalize() for types in pokemon['types']]
+    pokemon['types_string'] = ' | '.join(types)
+    height = pokemon['height'] / 10
+    weight = pokemon['weight'] / 10
+    pokemon['height_mts'] = height
+    pokemon['weight_kg'] = weight
+    return pokemon
+
+
 
 def getAllPokemons(amount_pokemons):
-  pass
+    contador = 0
+    limit = amount_pokemons
+    url = f"https://pokeapi.co/api/v2/pokemon/?limit={limit}"
+    response = requests.get(url)
+    jsonObject = response.json()
+    pokemonsList = jsonObject['results']
+    for pokemon in pokemonsList:
+        contador += 1
+        img = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{contador}.png"
+        pokemon['id'] = f"{contador}"
+        pokemon['img'] = img
 
-def term_verify(pokemon, term, array):
-  pass
+    return pokemonsList
 
-def getPokemonSpecie(url_specie):
-  pass
+
+
+def termo_verify(pokemon, term, array):
+    if pokemon['name'].find(term) != -1 or pokemon['id'].find(term) != -1:
+      array.append(pokemon)
+
+
+def getPokemonSpecie(url):
+  response = requests.get(url)
+  specie = response.json()
+  return specie
 
 def getPokemonDescription(array):
-  pass
+  for item in array:
+    if item['language']['name'] == 'en':
+      string = item['flavor_text']
+      break
+
+  string = string.replace('\n', ' ')
+  string = string.replace('\f', ' ')
+  string = string.replace('POKéMON', 'pokemon') 
+  return string
 
 def getPokemonHabitat(specie):
-  pass
+  habitat = 'Uninformed' if not specie['habitat'] else specie['habitat']['name'].capitalize()
+  return habitat
 
 def getPokemonGrowthRate(specie):
-  pass
+  growth_rate = 'Uninformed' if not specie['growth_rate'] else specie['growth_rate']['name'].capitalize()
+  return growth_rate
 
 def getPokemonShape(specie):
-  pass
+  shape = 'Uninformed' if not specie['shape'] else specie['shape']['name'].capitalize()
+  return shape
 
-def getPokemonType(url_type, item):
-  pass
+
+def getPokemonType(url, item):
+  # Fazendo o get da url
+  response = requests.get(url)
+
+  # Transoformando a resposta em JSON e armazenando na variável type
+  type = response.json()
+
+  # Criando a variável que receberá o array com os objetos para varrer.
+  array_weakness = type['damage_relations'][item]
+
+  # Criando a variável final que terá a lista somente com os nomes das fraquezas 
+  list = getPokemonWeakness(array_weakness)
+
+  # Adicionando um item na variável Type com a lista das fraquezas
+  type['list'] = list
+ 
+  # Retorno o type para ser usado na minha página.
+  return type
+
+
 
 def getPokemonWeakness(array):
-  pass
+  # Criando um array vazio para preencher com os nomes das fraquezas
+  all_weakness = []
+
+  # Passando por cada item do array, recolhendo os nomes e adicionando a lista vazia
+  for item in array:
+    all_weakness.append(item['name'])
+   
+  # retorno a lista com os nomes após preenchê-la
+  return all_weakness
   
--- pokedexApp/views.py
+++ pokedexApp/views.py
@@ -1,12 +1,86 @@
 from django.views.generic.base import TemplateView
-from django.core import paginator
+from django.core.paginator import Paginator
+from .utils import *
 import requests
 
 class HomeView(TemplateView):
-  pass
+    template_name = "myPokedex/index.html"
 
-class PokemonView(HomeView):
-  pass
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pokemonsList = getAllPokemons(898)
+        paginate_by = 12
 
-class BuscaView(HomeView):
-  pass
+        paginator = Paginator(pokemonsList, paginate_by)
+        page = self.request.GET.get('page')
+        pokemonsList = paginator.get_page(page)
+
+        pokemons = [getPokemon(pokemon['url']) for pokemon in pokemonsList]
+
+        context['pokemonsList'] = pokemonsList
+        context['pokemons'] = pokemons
+        return context
+
+
+
+
+class PokemonView(TemplateView):
+    template_name = 'myPokedex/pokemon.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pokemonID = self.request.GET.get('pokemonID')  
+        pokemon = getPokemon(f"https://pokeapi.co/api/v2/pokemon/{pokemonID}")
+        specie = getPokemonSpecie(pokemon['species']['url'])
+        weakness_list = []
+        strenghts_list = []
+
+        for types in pokemon['types']:
+          url = types['type']['url']
+          weakness = getPokemonType(url, 'double_damage_from')          
+          weakness_list.append(weakness['list'])
+          strenghts = getPokemonType(url, 'double_damage_to')    
+          strenghts_list.append(strenghts['list'])
+
+
+
+        description_list = specie['flavor_text_entries']
+        description_formated = getPokemonDescription(description_list)
+        pokemon['description'] = description_formated
+        pokemon['habitat'] = getPokemonHabitat(specie)
+        pokemon['growth_rate'] = getPokemonGrowthRate(specie)
+        pokemon['shape'] = getPokemonShape(specie)
+        pokemon['weakness_list'] = weakness_list
+        pokemon['strenghts_list'] = strenghts_list
+
+        context['pokemonID'] = pokemonID      
+        context['pokemon'] = pokemon
+        context['specie'] = specie
+        return context
+
+
+
+class BuscaView(TemplateView):
+    template_name = 'myPokedex/pokemon_busca.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        termo = self.request.GET.get('termo')
+        paginated_by = 12
+
+        allPokemons = getAllPokemons(898)
+        pokemonsList = []
+        for pokemon in allPokemons:
+          termo_verify(pokemon, termo, pokemonsList)
+
+
+        paginator = Paginator(pokemonsList, paginated_by)
+        page = self.request.GET.get('page')
+        pokemonsList = paginator.get_page(page)
+
+        pokemons = [getPokemon(pokemon['url']) for pokemon in pokemonsList]
+
+        context['pokemons'] = pokemons
+        context['pokemonsList'] = pokemonsList
+
+        return context
