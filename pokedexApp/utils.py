776f0adf30b7d675147b470ca40221d937d6e812
Lucas de Almeida Sarmento <lukas.barcee@gmail.com>
2021-10-07 23:04:24 -0300

    Adicionando funções de dividir listas e remover duplicados dos tipos de fraqueza e força.

--- pokedexApp/utils.py
+++ pokedexApp/utils.py
@@ -27,25 +27,26 @@ def get_all_pokemons(amount_pokemons:int) -> list:
   response = requests.get(url)
   jsonObject = response.json()
   pokemonsList = jsonObject['results']
+
   for pokemon in pokemonsList:
     counter += 1
     img = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{counter}.png"
     pokemon['id'] = f"{counter}"
     pokemon['img'] = img
-
   return pokemonsList
 
 
 
-def term_verifyer(pokemon:dict, term:str, array:list):
-  if pokemon['name'].find(term) != -1 or pokemon['id'].find(term) != -1:
-    array.append(pokemon)
-
+def term_verifyer(pokemon:dict, term:str, array_accumulator:list):
+  if term in pokemon['name'] or term in pokemon['id']:
+    array_accumulator.append(pokemon)
+    
 
 def get_pokemon_specie(url_specie:str) -> dict:
   response = requests.get(url_specie)
   specie = response.json()
   return specie
+
 
 def get_pokemon_description(list_of_descriptions:list) -> str:
   for description in list_of_descriptions:
@@ -57,13 +58,17 @@ def get_pokemon_description(list_of_descriptions:list) -> str:
   pokemon_description = pokemon_description.replace('POKéMON', 'pokemon') 
   return pokemon_description
 
+
+
 def get_pokemon_habitat(specie:dict) -> str:
   habitat = 'Uninformed' if not specie['habitat'] else specie['habitat']['name'].capitalize()
   return habitat
 
+
 def get_pokemon_growth_rate(specie:dict) -> str:
   growth_rate = 'Uninformed' if not specie['growth_rate'] else specie['growth_rate']['name'].capitalize()
   return growth_rate
+
 
 def get_pokemon_shape(specie:dict) -> str:
   shape = 'Uninformed' if not specie['shape'] else specie['shape']['name'].capitalize()
@@ -82,14 +87,28 @@ def get_pokemon_type(url_type:str) -> dict:
 
   type['list_weakness'] = list_weakness
   type['list_strenghts'] = list_strenghts
-
   return type
   
+
 
 def get_pokemon_weakness_or_strenghts(list:list) -> list:
   list_items = [item['name'] for item in list]   
   return list_items
+
+
+def split_lists_in_list(list_values:list) -> list:
+  new_list = []
+  for array in list_values:
+    for item in array:
+      new_list.append(item)
+  return new_list
+
+
+def remove_duplicates(list_values:list) -> list:
+  new_list = list(dict.fromkeys(list_values))
+  return new_list
   
+
 
 def get_pokemon_evolutions(specie:dict ) -> dict:
   evolutions_pokemon = {}
--- pokedexApp/views.py
+++ pokedexApp/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.base import TemplateView
+from django.shortcuts import redirect
 from django.core.paginator import Paginator
 from .utils import *
 
@@ -39,6 +40,9 @@ class PokemonView(TemplateView):
       type = get_pokemon_type(url)          
       weakness_list.append(type['list_weakness'])
       strenghts_list.append(type['list_strenghts'])
+    
+    weakness_list = split_lists_in_list(weakness_list)
+    strenghts_list = split_lists_in_list(strenghts_list)
 
     pokemon_evolutions = get_pokemon_evolutions(specie)
     evolution_one = pokemon_evolutions['evolution_one']
@@ -46,22 +50,21 @@ class PokemonView(TemplateView):
     if pokemon_evolutions['evolves']:
       context['pokemon_evolution_two'] = pokemon_evolutions['evolution_two']      
       if pokemon_evolutions['more_than_two_evolutions']:
-        context['pokemon_evolution_three'] = pokemon_evolutions['evolution_three']
-
-
+        context['pokemon_evolution_three'] = pokemon_evolutions['evolution_three']    
 
     description_list = specie['flavor_text_entries']
     description_formated = get_pokemon_description(description_list)
+
     pokemon['description'] = description_formated
     pokemon['habitat'] = get_pokemon_habitat(specie)
     pokemon['growth_rate'] = get_pokemon_growth_rate(specie)
     pokemon['shape'] = get_pokemon_shape(specie)
-    pokemon['weakness_list'] = weakness_list
-    pokemon['strenghts_list'] = strenghts_list
+
+    context['weakness_list'] = remove_duplicates(weakness_list)
+    context['strenghts_list'] = remove_duplicates(strenghts_list)
     context['pokemon_evolution_one'] = evolution_one
     context['pokemon_evolves'] = pokemon_evolutions['evolves']
     context['evolves_three_times'] = pokemon_evolutions['more_than_two_evolutions']
-
     context['pokemonID'] = pokemonID      
     context['pokemon'] = pokemon
     context['specie'] = specie
@@ -70,26 +73,26 @@ class PokemonView(TemplateView):
 
 
 class BuscaView(TemplateView):
-    template_name = 'pokedexApp/pokemon_busca.html'
+  template_name = 'pokedexApp/pokemon_busca.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        termo = self.request.GET.get('termo')
-        paginated_by = 12
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    term = self.request.GET.get('term')
+    paginated_by = 12
 
-        allPokemons = get_all_pokemons(898)
-        pokemonsList = []
-        for pokemon in allPokemons:
-          term_verifyer(pokemon, termo, pokemonsList)
+    allPokemons = get_all_pokemons(898)
+    pokemonsList = []
+    for pokemon in allPokemons:
+      term_verifyer(pokemon, term, pokemonsList)
 
 
-        paginator = Paginator(pokemonsList, paginated_by)
-        page = self.request.GET.get('page')
-        pokemonsList = paginator.get_page(page)
+    paginator = Paginator(pokemonsList, paginated_by)
+    page = self.request.GET.get('page')
+    pokemonsList = paginator.get_page(page)
 
-        pokemons = [get_pokemon(pokemon['url']) for pokemon in pokemonsList]
+    pokemons = [get_pokemon(pokemon['url']) for pokemon in pokemonsList]
 
-        context['pokemons'] = pokemons
-        context['pokemonsList'] = pokemonsList
+    context['pokemons'] = pokemons
+    context['pokemonsList'] = pokemonsList
 
-        return context
+    return context
